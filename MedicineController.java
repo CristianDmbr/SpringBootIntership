526c3c37fce1b36d9fa783dbe06eded4070d8351
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2023-07-19 02:26:01 +0100

    Update MedicineController.java

--- MedicineController.java
+++ MedicineController.java
@@ -19,11 +19,13 @@ public class MedicineController {
     @Resource
     private final MedicineRepository medicineRepository;
 
+    //Method displaying all the data from the table.
     @GetMapping
     public ResponseEntity<Iterable<Medicine>> getAllMedicine() {
         return new ResponseEntity<>(medicineRepository.findAll(), HttpStatus.OK);
     }
 
+    //Method displaying specific data by id from the table.
     @GetMapping("/{id}")
     public ResponseEntity<Medicine> getMedicineById(@PathVariable long id) {
         Optional<Medicine> student = medicineRepository.findById(id);
@@ -35,17 +37,28 @@ public class MedicineController {
     }
 
 
+    //Methods that deletes data from table by ID.
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteMedicineById(@PathVariable long id) {
         medicineRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    //Method that all the data from the table.
+    @DeleteMapping("/all")
+    public ResponseEntity<HttpStatus> deleteAllMedicine() {
+        medicineRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    //Method that allows to insert data into the table.
     @PostMapping
     public ResponseEntity<Medicine> createMedicine(@RequestBody @Valid Medicine medicine) {
         return new ResponseEntity<>(medicineRepository.save(medicine), HttpStatus.CREATED);
     }
 
+    //Method updating the data from the table.
     @PutMapping
     public ResponseEntity<HttpStatus> updateMedicine(@RequestBody @Valid Medicine medicine) {
         medicineRepository.save(medicine);

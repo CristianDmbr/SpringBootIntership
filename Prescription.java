c62872714734979cee1d6e025b71f5c119b9a3f3
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2023-07-20 01:51:28 +0100

    Update Prescription.java

--- Prescription.java
+++ Prescription.java
@@ -3,6 +3,7 @@ package com.example.backend.model;
 import jakarta.persistence.*;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,7 +22,7 @@ public class Prescription {
     private long id;
 
     @NotNull(message = "Gender cannot be null")
+    @Size(min = 2, max = 30, message = "The minimum and maximum lengths are 2 and 30 for adding the producer name.")
     @Column(name = "medicineType")
     private String medicineType;
-
 }

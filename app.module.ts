d2b55829ad55f215c1016c5027b02053e1031bf6
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2023-07-22 12:28:18 +0100

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicineComponent } from './medicine/medicine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicineComponent,
+    // Remove MedicineTableComponent from the declarations array
+    // MedicineTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

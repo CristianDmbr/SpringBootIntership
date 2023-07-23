e1fda89b4cb01ddaf2b4dfd0e4e3dfa4e29f34a7
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2023-07-23 16:27:30 +0100

    Update MedicineController.java

--- MedicineController.java
+++ MedicineController.java
@@ -1,14 +1,18 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.Medicine;
+import com.example.backend.model.Prescription;
 import com.example.backend.repository.MedicineRepository;
+
+import com.example.backend.repository.PrescriptionRepository;
 import jakarta.annotation.Resource;
-import jakarta.validation.Valid;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -17,10 +21,14 @@ import java.util.Optional;
 public class MedicineController {
 
     @Resource
+    private final PrescriptionRepository prescriptionRepository;
+
+    @Resource
     private final MedicineRepository medicineRepository;
 
     //Method displaying all the data from the table.
     @GetMapping
+    @ResponseBody
     public ResponseEntity<Iterable<Medicine>> getAllMedicine() {
         return new ResponseEntity<>(medicineRepository.findAll(), HttpStatus.OK);
     }
@@ -28,13 +36,15 @@ public class MedicineController {
     //Method displaying specific data by id from the table.
     @GetMapping("/{id}")
     public ResponseEntity<Medicine> getMedicineById(@PathVariable long id) {
-        Optional<Medicine> student = medicineRepository.findById(id);
-        if (student.isPresent()) {
-            return new ResponseEntity<>(student.get(), HttpStatus.OK);
+        Optional<Medicine> medicine = medicineRepository.findById(id);
+        if (medicine.isPresent()) {
+            return new ResponseEntity<>(medicine.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+
 
 
     //Methods that deletes data from table by ID.
@@ -55,13 +65,33 @@ public class MedicineController {
     //Method that allows to insert data into the table.
     @PostMapping
     public ResponseEntity<Medicine> createMedicine(@RequestBody @Valid Medicine medicine) {
+        if (medicine.getPrescription() != null) {
+            Prescription prescription = medicine.getPrescription();
+            if (prescription.getId() == 0) {
+                // If the prescription ID is 0, it means the prescription is not saved in the database.
+                Prescription savedPrescription = prescriptionRepository.save(prescription);
+                medicine.setPrescription(savedPrescription);
+            } else {
+                // If the prescription ID is not 0, it means the prescription might already exist in the database.
+                // In this case, just fetch the existing prescription by ID and associate it with the medicine.
+                Optional<Prescription> prescriptionOptional = prescriptionRepository.findById(prescription.getId());
+                if (prescriptionOptional.isPresent()) {
+                    Prescription existingPrescription = prescriptionOptional.get();
+                    medicine.setPrescription(existingPrescription);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+            }
+        }
+
         Medicine savedMedicine = medicineRepository.save(medicine);
         return new ResponseEntity<>(savedMedicine, HttpStatus.CREATED);
     }
 
 
+
     //Method updating the data from the table.
-    @PutMapping
+    @PutMapping("/medicines{id}/edit")
     public ResponseEntity<HttpStatus> updateMedicine(@RequestBody @Valid Medicine medicine) {
         medicineRepository.save(medicine);
         return new ResponseEntity<>(HttpStatus.OK);

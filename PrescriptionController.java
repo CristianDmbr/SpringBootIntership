ad688628ec9d4b1d6c5cd3361e16eed4562ecf5c
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2023-07-23 16:19:29 +0100

    Update PrescriptionController.java

--- PrescriptionController.java
+++ PrescriptionController.java
@@ -2,10 +2,21 @@ package com.example.backend.controller;
 
 import com.example.backend.model.Prescription;
 import com.example.backend.repository.PrescriptionRepository;
+
 import jakarta.annotation.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/prescription")
@@ -29,16 +40,12 @@ public class PrescriptionController {
     public ResponseEntity<HttpStatus> updatePrescription(@PathVariable long id, @RequestBody Prescription updatedPrescription) {
         Prescription prescription = prescriptionRepository.findById(id).orElse(null);
         if (prescription != null) {
-            // Update the prescription with the new data
+            // Updates the prescription with the new data
             prescription.setMedicineType(updatedPrescription.getMedicineType());
-
-
-            // Save the updated prescription
             prescriptionRepository.save(prescription);
 
             return new ResponseEntity<>(HttpStatus.OK);
         } else {
-            // Prescription with the given ID not found
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
